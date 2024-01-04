0cbb60f26bbe3244c4a6037a496957bc148f72a4
mihail <artmea73@yahoo.com>
2024-01-04 21:11:06 +0300

    4.1. Разбор первой программы [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Mihail Artukhin");
         System.out.println("12.12.1973");
     }
