97d17781d3838e374a87c56a48d38bb99f4e7398
mihail <artmea73@yahoo.com>
2024-01-22 19:30:37 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -8,6 +8,6 @@ public class Point {
 
     public static void main(String[] args) {
         double result = Point.distance(1, 5, 2, 3);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        System.out.println("result (1, 5) to (2, 3) " + result);
     }
 }
