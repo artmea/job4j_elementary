57ee25f4a2969f8d77e8640e43f3ff2abb71c1a4
mihail <artmea73@yahoo.com>
2024-10-13 20:06:25 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -1,7 +1,10 @@
 package ru.job4j.loop;
 
 public class Fitness {
-    /*    public static int calc(int ivan, int nik) {
+    public static int calc(int ivan, int nik) {
+      if (ivan <= 0 || nik <= 0) {
+            throw new IllegalArgumentException("Сила Ивана и Ника должна быть больше 0");
+            }
             int month = 0;
             while (ivan <= nik) {
                 System.out.println(month);
@@ -12,19 +15,4 @@ public class Fitness {
             return month;
         }
     }
-    */
-    public static int calc(int ivan, int nik) {
-        if (ivan <= 0 || nik <= 0) {
-            throw new IllegalArgumentException("Сила Ивана и Ника должна быть больше 0");
-        }
-
-        int month = 0;
-        while (ivan <= nik) {
-            ivan *= 3;
-            nik *= 2;
-            month++;
-        }
-
-        return month;
-    }
-}
+// Строку 5,6 добавил chatgpt
