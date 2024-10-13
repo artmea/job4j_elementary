ce0098e9b40de32cdd44a0f6e8b360d2816a9f5d
mihail <artmea73@yahoo.com>
2024-10-13 19:54:55 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -1,14 +1,30 @@
 package ru.job4j.loop;
 
 public class Fitness {
+    /*    public static int calc(int ivan, int nik) {
+            int month = 0;
+            while (ivan <= nik) {
+                System.out.println(month);
+                ivan *= 3;
+                nik *= 2;
+                month++;
+            }
+            return month;
+        }
+    }
+    */
     public static int calc(int ivan, int nik) {
+        if (ivan <= 0 || nik <= 0) {
+            throw new IllegalArgumentException("Сила Ивана и Ника должна быть больше 0");
+        }
+
         int month = 0;
         while (ivan <= nik) {
-            System.out.println(month);
             ivan *= 3;
             nik *= 2;
             month++;
         }
+
         return month;
     }
 }
